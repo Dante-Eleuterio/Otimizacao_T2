ed47c339b7f5eddcb9d1570c259efae8238ec93b
Dante-Eleuterio <danteeleuterio00@gmail.com>
2023-06-26 14:20:09 -0300

    base done

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ vector<int> A,B,optimalA,optimalB,C;
 int nodes=0;
 int optimal=INT_MAX;
 int optimalcut =0; 
-int viabilitycut=1;
-
+int viabilitycut=0;
+int myfunction=0;
 
 int countconflicts(){
     int conflicts=0;
@@ -58,7 +58,6 @@ int checksolution(){
                 foundB=1;
             }
             if(foundA && foundB){
-                cout <<"erro\n";
                 return 0;
             }
         }
@@ -91,16 +90,16 @@ void bnb(int count){
 
     nodes++;
 
-    for (int i = 0; i < heroes[count].friends.size(); i++){
-        if(heroes[count].friends[i]->group==1){
-            foundA=1;
-        }
+    if(viabilitycut){
+        for (int i = 0; i < heroes[count].friends.size(); i++){
+            if(heroes[count].friends[i]->group==1){
+                foundA=1;
+            }
 
-        if(heroes[count].friends[i]->group==2){
-            foundB=1;
-        }
-        
-        if(viabilitycut){
+            if(heroes[count].friends[i]->group==2){
+                foundB=1;
+            }
+            
             if(foundA && foundB){
                 return;
             }
@@ -180,6 +179,14 @@ void bnb(int count){
 int main(int argc, char const *argv[]){
     
     int Nheroes,Nenemies,Nfriends,h1,h2;
+    for (int i = 0; i <argc; i++){
+        if(!strncmp(argv[i],"-a",3))
+            myfunction=0;
+        if(!strncmp(argv[i],"-f",3))
+            viabilitycut=1;
+        if(!strncmp(argv[i],"-o",3))
+            optimalcut=0;
+    }
     cin >> Nheroes >> Nenemies >> Nfriends;
     for (int i = 0; i < Nheroes; i++){
         graph aux;
@@ -199,17 +206,7 @@ int main(int argc, char const *argv[]){
         heroes[h2-1].friends.push_back(&heroes[h1-1]);
     }
 
-    // for (int i = 0; i < Nheroes; i++){
-    //     for (int j = 0;j< heroes[i].enemies.size(); j++){
-    //         cout<<heroes[i].name<<"->"<<heroes[i].enemies[j]->name<<"\n";
-    //     }
-    // }
-    // for (int i = 0; i < Nheroes; i++){
-    //     for (int j = 0;j< heroes[i].friends.size(); j++){
-    //         cout<<heroes[i].name<<"->"<<heroes[i].friends[j]->name<<"\n";
-    //     }
-    // }
-    
+   
     bnb(0);
     cout<<"Nodes= "<<nodes<<"\n";
     cout<<"Optimal= "<<optimal<<"\n";
